c54b3ed371dd3d8c8fdd2528059cf31b3ff551bf
Kamil Ronewicz <kamil.ronewicz@x-team.com>
2018-03-05 09:45:12 +0100

    Initialize playground repository

---
+++ javascript/vue/js/main.js
@@ -0,0 +1,31 @@
+Vue.component('todo-item', {
+  template: '<li>{{todo.text}}</li>',
+  props: ['todo']
+})
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Kamil, love\'s Vue.js'
+  }
+})
+
+const form = new Vue({
+  el: '#form',
+  data: {
+    name: 'Input element'
+  }
+})
+
+const todos = new Vue({
+  el: '#todos',
+  data: {
+    kamil: 'jak tam',
+    groceryList: [
+      { id: 1, text: 'hello' },
+      { id: 2, text: 'Next item' }
+    ]
+  }
+})
+
+todos.$watch('kamil', () => console.log('zmiana'))
