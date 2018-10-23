5ab3bc98b066c4be9ac71816ade249355ea676e3
Kamil Ronewicz <galileox86@gmail.com>
2018-10-23 22:44:51 +0200

    New blog header

--- javascript/gatsby/blog/src/pages/index.js
+++ javascript/gatsby/blog/src/pages/index.js
@@ -1,3 +1,40 @@
 import React from "react"
+import { StaticQuery, graphql } from 'gatsby'
 
-export default () => <div>Hello world!</div>
+const TitleDescription = ({data: {site: {siteMetadata: {title, description}}}}) => <div style={{
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+	fontFamily: 'avenir'
+}}
+>
+	<h2 style={{marginBottom: 0}}>{title}</h2>
+	<p style={{
+		marginTop: 0,
+		opacity: 0.5
+	}}>{description}</p>
+</div>
+
+const Header = () => {
+	return (
+		<StaticQuery
+			query={graphql`
+				query {
+					site {
+						siteMetadata {
+							title
+							description
+						}
+					}
+				}
+			`}
+			render={data => <TitleDescription data={data} />}
+		/>
+	)
+}
+
+const Layout = () => <div>
+<Header />
+</div>
+
+export default Layout
