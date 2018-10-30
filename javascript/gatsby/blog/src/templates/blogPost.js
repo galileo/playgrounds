9bb2ed6bae16da66b83c53e42ee33539d2f1155e
Kamil Ronewicz <galileox86@gmail.com>
2018-10-30 19:55:08 +0100

    Retreving additional informations for each pag

--- javascript/gatsby/blog/src/templates/blogPost.js
+++ javascript/gatsby/blog/src/templates/blogPost.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import {graphql} from 'gatsby'
 
-const Template = (props) => {
+const Template = ({data}) => {
+  const {html, frontmatter: {title}} = data.markdownRemark
+
   return (
     <div>
-      Blog Post Here
+      <h1>{title}</h1>
+      <div dangerouslySetInnerHTML={{__html: html}}/>
     </div>
   )
 }
+
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark( frontmatter: { path: { eq: $pathSlug } }) {
+      html 
+      frontmatter {
+        title
+      }
+    }
+  }
+`
 
 export default Template
